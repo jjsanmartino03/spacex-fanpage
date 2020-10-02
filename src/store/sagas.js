c6ff94b8370811c01a76f530c31c08a926fc282b
Julián Sanmartino <jjsanmartino03@gmail.com>
2020-10-02 16:56:29 -0300

    Move formatDate method to another file

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,69 +1,12 @@
 import axios from "axios";
 import { takeEvery, call, put } from "redux-saga/effects";
+
 import Launch from "../entities/Launch";
-import { actionNames, addUpcomingLaunches } from "./actions";
-
-
-const formatDateBasedOnPrecision = (date, precision) => {
-  // This method modifies the visible data based on the date precision of the launch
-  let commonOptions = {
-    year: "numeric",
-    hour12: false,
-    month: "short",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    timeZoneName: "short",
-  };
-  const returnFunction = (options) => {
-    return date.toLocaleString(
-      "default",
-      options
-    );
-  }
-
-  switch (precision) {
-    case "hour":
-      return returnFunction({
-        ...commonOptions,
-        weekday: "short"
-      });
-
-    case "day":
-      return returnFunction({
-        ...commonOptions,
-        hour: undefined,
-        minute: undefined,
-        timeZone: "UTC"
-      });
-
-    case "month":
-      return returnFunction({
-        ...commonOptions,
-        hour: undefined,
-        minute: undefined,
-        day: undefined,
-        timeZone: "UTC",
-      }).replace(", UTC", "");
-
-    case "year":
-      return returnFunction({
-        year: "numeric",
-        timeZone: "UTC",
-      });
-
-    case "quarter":
-      let quarter = Math.floor((date.getUTCMonth() + 1) / 3);
-      let year = date.getUTCFullYear();
-      return `Quarter ${quarter} ${year}`;
-
-    case "half":
-      return ((date.getUTCMonth() + 1) / 6 > 1 ? "1st Half " : "2nd Half ") + date.getUTCFullYear();
-
-    default:
-      return date.toString();
-  }
-}
+import {
+  actionNames,
+  addUpcomingLaunches
+} from "./actions";
+import { formatDateBasedOnPrecision } from "./formatDate";
 
 function* mainSaga() {
   yield takeEvery(actionNames.fetchUpcomingLaunches, fetchUpcomingSaga); // catch the action "fetchUpcomingFromApi"
