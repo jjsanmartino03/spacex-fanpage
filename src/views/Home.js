c49e8a52dbb31209043e0366c9b1d810ebe81734
Julián Sanmartino <jjsanmartino03@gmail.com>
2020-10-03 15:27:51 -0300

    Finish home-header and home-content

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Container } from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { changeActiveAnchor } from "../store/actions";
 
@@ -11,13 +11,21 @@ class HomeView extends React.Component {
   }
   render = () => {
     return (
-      <div className="home-view">
+      <div className="home-view bg-white">
         <Container className="home-header pb-big"
           fluid>
           <h1 className="text-center">SpaceX Fanpage</h1>
-          <h3 className="text-center pt-5 w-75">Do you like rockets? If so, you are in the right place! <span className="highlight">SpaceX Fanpage</span> is a personal project I made to show info about the company SpaceX. Until now it just displays the upcoming launches, and it gets the data from r-spacex/SpaceX-API, a public 'database' with live info of the company. Hope you like it!</h3>
+          <h4 className="text-center pt-5 w-75">Do you like rockets? If so, you are in the right place! <span className="highlight">SpaceX Fanpage</span> is a personal project I made to show info about the company SpaceX. Until now it just displays the upcoming launches, and it gets the data from <a target="_blank" href="https://github.com/r-spacex/SpaceX-API">r-spacex/SpaceX-API</a>, a public 'database' with live info of the company. Hope you like it!</h4>
         </Container>
-      </div>
+        <Container className="bg-white shadow rounded home-content py-4 mb-4 px-5 w-90-sm">
+          <h2>The project</h2>
+          <p>SpaceX Fanpage is a website that has some info about Space Exploration Technologies Inc (SpaceX), like upcoming launches. Up to now, October 2020, I've only made the Upcoming Launches page. The project itself has no backend, and it gets all the data from <a target="_blank" href="https://github.com/r-spacex/SpaceX-API">r-spacex/SpaceX-API</a>.</p>
+
+          <p>This is a practice project where I put in practice React, React-Bootstrap, React-Redux, Redux-Saga, tools I recently learned to use.</p>
+
+          <p>You can find</p>
+        </Container >
+      </div >
     )
   }
 }
