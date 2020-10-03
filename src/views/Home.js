ca26470e32d25c1536e0f981a35822a4a89a3029
Julián Sanmartino <jjsanmartino03@gmail.com>
2020-10-03 15:08:04 -0300

    Add home view and some css

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,21 +1,29 @@
 import React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
+import { Container } from "react-bootstrap";
+import {Link} from "react-router-dom";
 
-import {changeActiveAnchor} from "../store/actions";
+import { changeActiveAnchor } from "../store/actions";
 
-class HomeView extends React.Component{
-  componentDidMount = () =>{
+class HomeView extends React.Component {
+  componentDidMount = () => {
     this.props.dispatch(changeActiveAnchor("/"))
   }
   render = () => {
     return (
-      <h1>Hola mundo</h1>
+      <div className="home-view">
+        <Container className="home-header pb-big"
+          fluid>
+          <h1 className="text-center">SpaceX Fanpage</h1>
+          <h3 className="text-center pt-5 w-75">Do you like rockets? If so, you are in the right place! <span className="highlight">SpaceX Fanpage</span> is a personal project I made to show info about the company SpaceX. Until now it just displays the upcoming launches, and it gets the data from r-spacex/SpaceX-API, a public 'database' with live info of the company. Hope you like it!</h3>
+        </Container>
+      </div>
     )
   }
 }
 
-const mapStateToProps = ({}) => ({
-  
+const mapStateToProps = ({ }) => ({
+
 });
 
 export default connect(mapStateToProps)(HomeView);
